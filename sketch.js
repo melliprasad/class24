e6edb8d1b59eb78d64a794af95af21d6bd70bfde
melliprasad <80943840+melliprasad@users.noreply.github.com>
2021-05-17 16:35:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world, ground;
+var box1, box2, pig1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200, 100, 50, 50);
+    box2 = new Box(100, 50, 50, 100);
+
+    ground = new Ground(200, 390, 400, 20);
+
+    pig1 = new Pig(200, 200);
+    
+    console.log(pig1.x);
+}
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+}
